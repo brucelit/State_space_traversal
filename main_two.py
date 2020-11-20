e755c3b989cfa1c3d736937bd6eebd34d02a537c
brucelit <brucelit@163.com>
2020-11-20 10:33:19 +0100

    Reformat

---
+++ main_two.py
@@ -0,0 +1,25 @@
+ret_tuple_as_trans_desc = False
+
+from astar_implementation import utilities, thesis_two
+from pm4py.objects.petri import synchronous_product
+from pm4py.objects.petri.utils import decorate_places_preset_trans, decorate_transitions_prepostset
+
+
+model_net, model_im, model_fm = utilities.construct_model_net()
+trace_net, trace_im, trace_fm = utilities.construct_trace_net()
+sync_net, sync_im, sync_fm = synchronous_product.construct(trace_net, trace_im, trace_fm, model_net, model_im, model_fm,">>")
+cost_function = utilities.construct_cost_function(sync_net)
+decorate_transitions_prepostset(sync_net)
+decorate_places_preset_trans(sync_net)
+consumption_matrix = utilities.construct_consumption_matrix(sync_net)
+incidence_matrix, p_index, t_index = utilities.construct_incident_matrix(sync_net)
+possible_enabling_transitions = set()
+for p in sync_im:
+    for t in p.ass_trans:
+        if t.label[0] == t.label[1]:
+            a = t
+split_lst = [None,a]
+visited = 0
+align = thesis_two.astar_with_split(sync_net, sync_im, sync_fm, consumption_matrix, incidence_matrix, p_index, t_index, cost_function, split_lst, visited)
+
+
